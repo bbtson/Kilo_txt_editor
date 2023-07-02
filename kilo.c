a9fd8749f241181989766b096219125c16dbb8cb
bbtson <137317999+bbtson@users.noreply.github.com>
2023-07-02 21:05:39 +0300

    Add files via upload
    started implementing text editing

--- kilo.c
+++ kilo.c
@@ -236,6 +236,25 @@ void editorAppendRow(char *s, size_t len){
 
 }
 
+void editorRowInsertChar(erow *row, int at, int c){
+	if(at < 0 || row -> size) at = row -> size;
+	row->chars = realloc(row->chars, row->size + 2);
+	memmove(&row -> chars[at + 1], &row -> chars[at], row -> size - at + 1);
+	row->size++;
+	row->chars[at] = c;
+	editorUpdateRow(row);
+}
+
+/*** editor operations ***/
+
+void editorInsertChar(int c){
+	if(E.cy == E.numrows){
+		editorAppendRow("", 0);
+	}
+	editorRowInsertChar(&E.row[E.cy], E.cx, c);
+	E.cx++;
+}
+
 /*** file i/o ***/
 
 void editorOpen(char *filename){
@@ -472,6 +491,9 @@ void editorProcessKeypress(){
 		case ARROW_LEFT:
 		case ARROW_RIGHT:
 			editorMoveCursor(c);
+			break;
+		default:
+			editorInsertChar(c);
 			break;
 	}
 }
